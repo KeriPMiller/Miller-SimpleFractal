562230da271f3d205d9cc19dda080282e782999a
KeriPMiller <miller.keripatricia@gmail.com>
2017-12-16 09:33:07 -0500

    trying to figure out percentiles

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -17,12 +17,13 @@ class App extends Component {
     this.percentile = this.percentile.bind(this);
   }
 
+
   componentDidMount(){
-    axios.get('/api/employee/')
-    .then(res => res.data)
-    .then(employees => {
-      this.setState({employees})
-    });
+    // axios.get('/api/employee/')
+    // .then(res => res.data)
+    // .then(employees => {
+    //   this.setState({employees})
+    // });
 
     axios.get('/api/company/')
     .then(res => res.data)
@@ -42,26 +43,34 @@ class App extends Component {
     axios.get(`/api/employee/${event.target.candidateId.value}`)
     .then(res => res.data)
     .then( employee => {
+      this.percentile(employee, 2);
       this.setState({targetEmployee: employee})
     })
-    .catch(err => {console.log('not found!', err)});
-
-    // get similar candidates
-    console.log('submit',this.state.targetEmployee)
-     this.percentile(this.state.targetEmployee);
   }
 
-  getSimilarCanidates()
-  percentile(employee){
-    let totalScore;
-   axios.get(`/api/employee/company/${employee.company_id}`)
+  getSimilarCanidates(canidateCompany){
+    let companies = this.state.companies
+    // TODO: find company nead same fractal index and filterout the original employee co
+  }
+
+  percentile(employee, similarCompany){
+    // initialize with target employee stats
+    let otherEmployeesScores =[employee.score]
+    let totalScore = employee.score;
+    // getemployees at other company to compare
+   axios.get(`/api/employee/company/${similarCompany}`)
     .then(res => res.data)
     .then( otherEmployees =>  {
-        otherEmployees.map(singleEmployee => {
-          totalScore += singleEmployee.score;
-        })
+        this.setState({similarCanidates: otherEmployees})
       })
+      // get an array of other employee scores and the total to find the percentile
+    this.state.similarCanidates.map((canidate => {
+      otherEmployeesScores.push(canidate.score);
+      totalScore += canidate.score;
+    }))
 
+    let percentile = Math.round(totalScore/otherEmployeesScores.length)
+      console.log( otherEmployeesScores.sort(), "total", percentile)
   }
 
   render() {
@@ -77,7 +86,9 @@ class App extends Component {
               value={this.state.candidateId}
               onChange={this.handleChange} ></input>
               <input type='submit' value='submit' />
+              {/*i would move this out to another hidden component <Message/>*/}
               <div className="messageBox">
+                <p> click submit twice (for now)</p>
                 <h3>this candidate is in the {this.state.percentile}
                     percentile for the role of {this.state.targetEmployee.title} compaired to similar companies</h3>
               </div>
